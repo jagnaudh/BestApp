7602616bef458c29feb5385d97a5d0b068c980f5
Default <j.bhatia@h6n-mac01-9.local>
2018-12-26 12:35:07 -0500

    Initial Commit

---
+++ Gratuity Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gratuity Calculator
+//
+//  Created by j.bhatia on 12/26/18.
+//  Copyright © 2018 j.bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
